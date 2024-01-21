63ae810e2b0a1175fd34af40f175741d9cf6acd4
KauaHenSilva <kaua.sbc@gmail.com>
2024-01-21 17:33:33 -0300

    Commented out unnecessary code in _adicionar_StatusQuarto.h

--- funcoesBasicas(BackEnd)/quarto/AdicionarQuarto/inputsOnly/_adicionar_StatusQuarto.h
+++ funcoesBasicas(BackEnd)/quarto/AdicionarQuarto/inputsOnly/_adicionar_StatusQuarto.h
@@ -1,7 +1,7 @@
 #if !defined(_ADICIONARSTATUSQUARTO)
 #define _ADICIONARSTATUSQUARTO
 
-#include "../../../../utils/utilis.h"
+// #include "../../../../utils/utilis.h"
 
 static void initStatusQuarto(stDbQuarto *dbQuarto, int posicaoMemoria);
 // static void adicionarStatusQuartoUser(stDbQuarto *dbQuarto, int posicaoMemoria);
@@ -9,27 +9,27 @@ static void initStatusQuarto(stDbQuarto *dbQuarto, int posicaoMemoria);
 void adicionarStatusQuarto(stDbQuarto *dbQuarto, int posicaoMemoria)
 {
 
-  int ver;
-  printf("Digite o modelo de status do quarto [%d]: \n", dbQuarto[posicaoMemoria].numero);
-  printf("1 - status Padrao (Padrao: Livre)\n");
+  // int ver;
+  // printf("Digite o modelo de status do quarto [%d]: \n", dbQuarto[posicaoMemoria].numero);
+  // printf("1 - status Padrao (Padrao: Livre)\n");
   // printf("2 - status Personalizado\n");
   
-  Utils.InputsBasic.getNumeroInt(&ver, "Digite o modelo de status do quarto: ");
+  // Utils.InputsBasic.getNumeroInt(&ver, "Digite o modelo de status do quarto: ");
 
-  switch (ver)
-  {
-    case 1:
+  // switch (ver)
+  // {
+  //   case 1:
       initStatusQuarto(dbQuarto, posicaoMemoria);
-      break;
+      // break;
     // case 2:
     //   adicionarStatusQuartoUser(dbQuarto, posicaoMemoria);
     //   break;
-    default:
-      Utils.SystemComand.clearTela();
-      printf("Opcao anterior invalida!!\n");
-      adicionarStatusQuarto(dbQuarto, posicaoMemoria);
-      break;
-  }
+  //   default:
+  //     Utils.SystemComand.clearTela();
+  //     printf("Opcao anterior invalida!!\n");
+  //     adicionarStatusQuarto(dbQuarto, posicaoMemoria);
+  //     break;
+  // }
 }
 
 static void initStatusQuarto(stDbQuarto *dbQuarto, int posicaoMemoria)
