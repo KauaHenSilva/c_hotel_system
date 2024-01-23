7c117df01e9ab1c6ef4d68c1371d906bf945bba6
KauaHenSilva <kaua.sbc@gmail.com>
2024-01-23 11:11:37 -0300

    Remove unused code in inputsSv.h

--- utils/inputsSv/inputsSv.h
+++ utils/inputsSv/inputsSv.h
@@ -2,16 +2,13 @@
 #define INPUTSSV
 
 #include "./_input_Cpf.h"
-#include "./_input_Name.h"
 #include "./_input_Rg.h"
 #include "./_input_Telefone.h"
 
 typedef struct {
-  void (*getName)(char *name, const char *msg);
   void (*getCpf)(char *cpf, const char *msg);
   void (*getRg)(char *rg, const char *msg);
   void (*getTelefone)(char *telefone);
-
 } stInputsSv;
 
 #endif // INPUTSSV
